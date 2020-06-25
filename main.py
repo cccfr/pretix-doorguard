cda3f16941338f3a70a010faa5c51adb7acd10c9
alf632 <alf632@alfi.li>
2020-06-25 17:26:37 +0200

    parameterization

--- main.py
+++ main.py
@@ -8,39 +8,41 @@ from io import BytesIO
 from PIL import Image
 import requests
 
-mateserver = "https://schwarzelunge.club.klaut.cloud"
-
-headers = {}
-with open("authHeader.json") as headerfile:
-    headers = json.load(headerfile)
-codes = {}
 lastSync = datetime.datetime.now()
+config = {}
+#config = {
+           "meteserver": "http://mete.cloud.cccfr", 
+           "organizers": {"hustepeter": {"teercafe": ["daily", "monthly", "yearly"]}
+           "headers": {"Authorization": "Token xxxxx"}
+         }
 
 def getCodes():
     global lastSync
     print("getting codes")
     codes = {}
-    res = requests.get("%s/api/v1/organizers/hustepeter/events/caferuss/checkinlists" %mateserver, headers=headers)
-    if res.status_code != 200:
-        print("error getting checkin lists")
-        return
-    lists = res.json()["results"]
-    for checkinlist in lists:
-        if checkinlist["name"] in ("daily, monthly, yearly"):
-            res = requests.get("%s/api/v1/organizers/hustepeter/events/caferuss/checkinlists/%s/positions" %(mateserver, checkinlist["id"]), headers=headers)
+    for organizer in config["organizers"].keys():
+        for event in config["organizers"][organizer].keys():
+            res = requests.get("%s/api/v1/organizers/%s/events/%s/checkinlists" %(config["meteserver"], organizer, event), headers=config["headers"])
             if res.status_code != 200:
-                continue
-            tickets = res.json()["results"]
-            for ticket in tickets:
-                codes[ticket["secret"]] = {"checkinlist": checkinlist["id"], "id": ticket["id"], "order": ticket["order"]}
+                print("error getting checkin lists")
+                return
+            lists = res.json()["results"]
+            for checkinlist in lists:
+                if checkinlist["name"] in config["organizers"][organizer][event]:
+                    res = requests.get("%s/api/v1/organizers/%s/events/%s/checkinlists/%s/positions" %(config["meteserver"], organizer, event, checkinlist["id"]), headers=config["headers"])
+                    if res.status_code != 200:
+                        continue
+                    tickets = res.json()["results"]
+                    for ticket in tickets:
+                        codes[ticket["secret"]] = {"checkinlist": checkinlist["id"], "id": ticket["id"], "organizer": organizer, "event": event, "order": ticket["order"]}
     lastSync = datetime.datetime.now()
     print("current codes: %s" %codes)
     return codes
 
 def checkinCode(code):
-    res = requests.post("%s/api/v1/organizers/hustepeter/events/caferuss/checkinlists/%s/positions/%s/redeem/" %(mateserver, code["checkinlist"], code["id"]), headers=headers)
+    res = requests.post("%s/api/v1/organizers/%s/events/%s/checkinlists/%s/positions/%s/redeem/" %(config["meteserver"], code["organizer"], code["event"], code["checkinlist"], code["id"]), headers=config["headers"])
     if res.status_code != 201:
-        print("cannot checkin ticket: %s" %code)
+        print("cannot checkin ticket: %s\n%s" %(code, res.text))
 
 def openDoor():
     print("opening...")
@@ -51,32 +53,38 @@ def openDoor():
 def signalLED():
     print("flashing led")
 
+def main():
+    global config
+    with open("config.json") as configfile:
+        config = json.load(configfile)
 
-codes = getCodes()
+    codes = {}
+    codes = getCodes()
 
-with picamera.PiCamera() as camera:
-    stream = BytesIO()
-    camera.start_preview()
-    time.sleep(2)
-    print("Scanner initialized")
-    while True:
-        timePassed = datetime.datetime.now() - lastSync
-        if timePassed.seconds > 10:
-            codes = getCodes()
-            lastSync = datetime.datetime.now()
-        stream.seek(0)
-        camera.capture(stream, format='jpeg', use_video_port=True)
-        stream.seek(0)
-        frame = Image.open(stream)
-        #frame.save("cam.png")
-        barcodes=decode(frame)
-        for barcode in barcodes:
-            signalLED()
-            data = barcode.data.decode("utf-8")
-            print("code read: %s" %data )
-            if data in codes.keys():
-                print("found valid ticket: %s" %codes[data])
-                checkinCode(codes[data])
-                openDoor()
+    with picamera.PiCamera() as camera:
+        stream = BytesIO()
+        camera.start_preview()
+        time.sleep(2)
+        print("Scanner initialized")
+        while True:
+            timePassed = datetime.datetime.now() - lastSync
+            if timePassed.seconds > 10:
+                codes = getCodes()
+                lastSync = datetime.datetime.now()
+            stream.seek(0)
+            camera.capture(stream, format='jpeg', use_video_port=True)
+            stream.seek(0)
+            frame = Image.open(stream)
+            #frame.save("cam.png")
+            barcodes=decode(frame)
+            for barcode in barcodes:
+                signalLED()
+                data = barcode.data.decode("utf-8")
+                print("code read: %s" %data )
+                if data in codes.keys():
+                    print("found valid ticket: %s" %codes[data])
+                    openDoor()
+                    checkinCode(codes[data])
 
-
+if __name__ == "__main__":
+    main()
