5d02fc4e1ac5ed25a66fb997633362cef73fc3c6
alf632 <alf632@alfi.li>
2020-07-03 00:40:26 +0200

    add systemd logging

--- main.py
+++ main.py
@@ -7,42 +7,63 @@ import png
 from io import BytesIO
 from PIL import Image
 import requests
+from systemd.journal import JournaldLogHandler
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+journald_handler = JournaldLogHandler()
+journald_handler.setFormatter(logging.Formatter(
+    '[%(levelname)s] %(message)s'
+))
+logger.addHandler(journald_handler)
+
+stdouthandler = logging.StreamHandler(sys.stdout)
+stdouthandler.setFormatter(logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+))
+logger.addHandler(stdouthandler)
+logger.setLevel(logging.INFO)
+
 
 lastSync = datetime.datetime.now()
 config = {}
 #config = {
-           "meteserver": "http://mete.cloud.cccfr", 
-           "organizers": {"hustepeter": {"teercafe": ["daily", "monthly", "yearly"]}
-           "headers": {"Authorization": "Token xxxxx"}
-         }
+#           "pretix": "http://mete.cloud.cccfr", 
+#           "organizers": {"hustepeter": {"teercafe": ["daily", "monthly", "yearly"]}
+#           "headers": {"Authorization": "Token xxxxx"}
+#         }
 
 def getCodes():
     global lastSync
-    print("getting codes")
+    logger.info("getting codes")
     codes = {}
     for organizer in config["organizers"].keys():
         for event in config["organizers"][organizer].keys():
-            res = requests.get("%s/api/v1/organizers/%s/events/%s/checkinlists" %(config["meteserver"], organizer, event), headers=config["headers"])
+            logger.debug("getting checkinlists for %s/%s" %(organizer, event))
+            res = requests.get("%s/api/v1/organizers/%s/events/%s/checkinlists" %(config["pretix"], organizer, event), headers=config["headers"])
             if res.status_code != 200:
-                print("error getting checkin lists")
-                return
+                logger.error("error getting checkin lists")
+                return {}
             lists = res.json()["results"]
+            logger.debug("found lists:\n%s" %lists)
             for checkinlist in lists:
+                logger.debug("if %s in %s\n%s" %(checkinlist["name"], config["organizers"][organizer][event], checkinlist["name"] in config["organizers"][organizer][event]))
                 if checkinlist["name"] in config["organizers"][organizer][event]:
-                    res = requests.get("%s/api/v1/organizers/%s/events/%s/checkinlists/%s/positions" %(config["meteserver"], organizer, event, checkinlist["id"]), headers=config["headers"])
+                    res = requests.get("%s/api/v1/organizers/%s/events/%s/checkinlists/%s/positions" %(config["pretix"], organizer, event, checkinlist["id"]), headers=config["headers"])
                     if res.status_code != 200:
                         continue
                     tickets = res.json()["results"]
                     for ticket in tickets:
                         codes[ticket["secret"]] = {"checkinlist": checkinlist["id"], "id": ticket["id"], "organizer": organizer, "event": event, "order": ticket["order"]}
     lastSync = datetime.datetime.now()
-    print("current codes: %s" %codes)
+    logger.info("current codes: %s" %codes)
     return codes
 
 def checkinCode(code):
-    res = requests.post("%s/api/v1/organizers/%s/events/%s/checkinlists/%s/positions/%s/redeem/" %(config["meteserver"], code["organizer"], code["event"], code["checkinlist"], code["id"]), headers=config["headers"])
+    res = requests.post("%s/api/v1/organizers/%s/events/%s/checkinlists/%s/positions/%s/redeem/" %(config["pretix"], code["organizer"], code["event"], code["checkinlist"], code["id"]), headers=config["headers"])
     if res.status_code != 201:
-        print("cannot checkin ticket: %s\n%s" %(code, res.text))
+        logger.error("cannot checkin ticket: %s\n%s" %(code, res.text))
 
 def openDoor():
     print("opening...")
@@ -55,21 +76,25 @@ def signalLED():
 
 def main():
     global config
+    global lastSync
     with open("config.json") as configfile:
         config = json.load(configfile)
 
-    codes = {}
     codes = getCodes()
+    if len(codes) == 0:
+        codes = {}
 
     with picamera.PiCamera() as camera:
         stream = BytesIO()
         camera.start_preview()
         time.sleep(2)
-        print("Scanner initialized")
+        logger.info("Scanner initialized")
         while True:
             timePassed = datetime.datetime.now() - lastSync
             if timePassed.seconds > 10:
-                codes = getCodes()
+                newcodes = getCodes()
+                if len(newcodes) > 0:
+                    codes = newcodes
                 lastSync = datetime.datetime.now()
             stream.seek(0)
             camera.capture(stream, format='jpeg', use_video_port=True)
@@ -80,9 +105,9 @@ def main():
             for barcode in barcodes:
                 signalLED()
                 data = barcode.data.decode("utf-8")
-                print("code read: %s" %data )
+                logger.info("code read: %s" %data )
                 if data in codes.keys():
-                    print("found valid ticket: %s" %codes[data])
+                    logger.info("found valid ticket: %s" %codes[data])
                     openDoor()
                     checkinCode(codes[data])
 
